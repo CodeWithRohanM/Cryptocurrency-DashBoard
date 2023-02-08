582847a87b37115c1076deafd8281e73b53f35f2
Rohan Mote <irohanmote@gmail.com>
2023-02-08 15:03:51 +0530

    Made Necessary Changes

--- src/Reducers/rootReducer.js
+++ src/Reducers/rootReducer.js
@@ -1,11 +1,9 @@
 import {combineReducers} from "redux";
 
 import callListAPIReducer from "./callListAPI";
-import whatsLoadingReducer from "./WhatsLoading";
 
 const rootReducer = combineReducers({
     callListAPIReducer,
-    whatsLoadingReducer,
 });
 
 export default rootReducer;
